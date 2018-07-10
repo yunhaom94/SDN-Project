4d61d6f456fb3818bc0f30619d0374bfa8af1a9c
Yunhao Mao <yunhaom94@outlook.com>
2018-07-10 16:50:46 -0400

    added ability to parse the result into .csv with result_parser.py

--- result_parser.py
+++ result_parser.py
@@ -1,7 +1,42 @@
 '''
-Parse the simulator switch output to an excel sheet(.xlsx) so that it would be easy to create a graph
+Parse the simulator switch output to an .csv so that it would be easy to create a graph
 '''
 import argparse
+import csv
+
+
+
+def parse(file_name):
+    csvfile_name = file_name + "_parsed.csv"
+    csvfile = open(csvfile_name, "w+", newline='')
+    headers = ["Time", "Active_Flows", "Hit_Rate"]
+    writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=headers)
+    writer.writeheader()
+
+    time = 0
+    order = 0 # the numbers are in order, i.e third one after * is active flows
+    tracking_values = {"Time":time}
+    with open(file_name, "r", encoding="utf-8") as f:
+        line = f.readline()
+        while line:
+            content = line.split(":")
+            if len(content) == 2:
+                value = content[1].strip()
+                if order == 3:
+                    tracking_values["Active_Flows"] = value
+                elif order == 6:
+                    tracking_values["Hit_Rate"] = value
+                
+                order += 1
+
+            if line.strip() == "*":
+                time += 100 # because we have a dump time of 100
+                order = 0
+                writer.writerow(tracking_values)
+                tracking_values = {"Time":time}
+
+            line = f.readline()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -9,16 +44,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     result_file = args.result_file
+    print("Parsing " + result_file)
+    parse(result_file)
+    print("Done")
 
-    with open(result_file, "r", encoding="utf-8") as f:
-        line = f.readline()
-        while line:
-            content = line.split(":")
-            if len(content) == 2:
-                value = content[1].strip()
-                print(value)
-
-            if line.strip() == "*":
-                print(line)
-
-            line = f.readline()
