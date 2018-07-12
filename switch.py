3452af6ab2352502f2d6393085c0bf9e004d739f
Yunhao Mao <yunhaom94@outlook.com>
2018-07-12 00:43:23 -0400

    updated switch to have a new data structure

--- switch.py
+++ switch.py
@@ -48,14 +48,13 @@ class Switch:
     def __init__(self, id, timeout, to_file):
         self.id = id
         self.flow_table = FlowTable(timeout)
-        self.controller = Controller()
         self.current_time = 0
-        self.dump_interval = 100 # hardcode to 10ms right now
+        self.dump_interval = timeout # should be same as timeout
         # last time statistic dump time
         # also used for check for timeout
         self.last_dump_time = 0 
         self.total_packets = 0
-        self.first_wirte = True # used to check for writing logs to file
+        self.first_write = True # used to check for writing logs to file
         self.missed = 0 # # of packets that needs to create a new flow for it
         self.output_to_file = to_file
 
@@ -118,15 +117,13 @@ class Switch:
         id = packet.get_id()
 
         if self.flow_table.if_flow_exists(id):
-            self.flow_table.update_flow(packet)
+            self.flow_table.existing_flow(packet)
         else:
-            flow = self.controller.create_flow(packet)
-            self.flow_table.insert_flow(flow)
-            self.missed += 1
+            self.flow_table.non_existing_flow(packet)
 
     def output_statistics(self, to_file):
 
-        hit_ratio = float((self.total_packets - self.missed) / self.total_packets)
+        hit_ratio = float((self.total_packets - self.flow_table.missed) / self.total_packets)
 
         output_str = '''
 Current Time is: {time}
@@ -135,7 +132,7 @@ Timeout Set to: {timeout}
 Currently Active Flows: {active_flow}
 Maximum Number of Packets In Active Flows: {max_packets}
 Maximum Number of Bytes In Active Flows: {max_bytes}
-Total Number of Rules Ever Installed: {total_flow}
+Total Number of Rules Ever Installed: {total_rules}
 Overall Hit Ratio: {hit_ratio}
 Maximum Number of Installed Rules At a Time: {max_flow_count}
 *
@@ -143,7 +140,7 @@ Maximum Number of Installed Rules At a Time: {max_flow_count}
         total_packet=str(self.total_packets),\
         timeout=str(self.flow_table.timeout),\
         active_flow=str(self.flow_table.current_active_flow),\
-        total_flow=str(self.flow_table.total_flow),\
+        total_rules=str(self.flow_table.total_rules),\
         max_flow_count=str(self.flow_table.max_flow_count),\
         max_packets=str(self.flow_table.get_max_packets_flow()),\
         max_bytes="TODO",\
@@ -152,10 +149,10 @@ Maximum Number of Installed Rules At a Time: {max_flow_count}
         if to_file:
             filename = "log_" + str(self.id)
             # create a file if first time writing to file
-            if self.first_wirte: 
+            if self.first_write: 
                 outfile = open(filename, "w+") # create or overwrite the file
                 outfile.close()
-                self.first_wirte = False
+                self.first_write = False
 
             with open(filename, "a") as out_file:
                 out_file.write(output_str)
@@ -175,80 +172,122 @@ class FlowTable:
         self.timeout_flow_count = 0
         self.max_flow_count = 0
         self.current_active_flow = 0
-        self.total_flow = 0
+        self.total_flows = 0
+        self.total_rules = 0
+        self.missed = 0
 
-    def if_flow_exists(self, flow_id):
-        if flow_id in self.table:
+    def if_flow_exists(self, id):
+        if id in self.table.keys():
             return True
         else:
             return False
 
-    def insert_flow(self, flow):
-        if self.if_flow_exists(flow.id):
-            raise Exception("Flow already exists")
-
-        self.table[flow.id] = flow
-        self.total_flow += 1
-        self.current_active_flow += 1
-        if self.current_active_flow > self.max_flow_count:
-            self.max_flow_count = self.current_active_flow
-
-    def delete_flow(self, id):
-        if not self.if_flow_exists(id):
-            raise Exception("Flow does not exist")
-        
-        del self.table[id]
-        self.current_active_flow -= 1
-
-    def update_flow(self, packet):
+    def existing_flow(self, packet):
         id = packet.get_id()
         if not self.if_flow_exists(id):
             raise Exception("Flow does not exist")
 
         flow = self.table[id]
-        flow.packets_count += 1
+        latest_rule = None
+
+        if flow.active:
+            latest_rule = flow.rules[-1]
+        else:
+            latest_rule = flow.create_rule(packet.timestamp)
+            self.missed += 1
+            self.total_rules += 1
+            self.current_active_flow += 1
+            flow.active = True
+            Output.DEBUG("Added new rule")
+
         flow.last_update = packet.timestamp
+        latest_rule.last_update = packet.timestamp
+        latest_rule.packets_count += 1
+        
         self.table[id] = flow
+
+
+    def non_existing_flow(self, packet):
+        id = packet.get_id()
+        if self.if_flow_exists(id):
+            raise Exception("Flow exists")
+
+        self.table[id] = self.create_flow(packet)
+        self.existing_flow(packet)
+        self.total_flows += 1
+
+
+    def deactivate_flow(self, id):
+        if not self.if_flow_exists(id):
+            raise Exception("Flow does not exist")
+        
+        self.table[id].active = False
+        self.current_active_flow -= 1
 
     def check_timeout(self, flow, current_time):
         """
         Iterate through the table and check for timeout
         """
 
-        # convert to ms
+        # converts to ms
         delta = (current_time - flow.last_update) * 1000
         if delta > self.timeout:
+            Output.DEBUG("Timing out " + flow.id)
             return True
         else:
             return False
     
     def all_timeout(self, current_time):
-        to_delete = []
+        expired = []
 
         for id, flow in self.table.items():
-            if self.check_timeout(flow, current_time):
-                to_delete.append(id)
+            if flow.active and self.check_timeout(flow, current_time):
+                expired.append(id)
 
-        for id in to_delete:
-            self.delete_flow(id)
+        for id in expired:
+            self.deactivate_flow(id)
 
     def get_max_packets_flow(self):
         
         if len(self.table.values()) > 0:
-            return max(self.table.values(), key=operator.attrgetter("packets_count")).packets_count
+            max_flow = max(self.table.values(), key=lambda x : x.rules[-1].packets_count)
+            return max_flow.rules[-1].packets_count
         else:
             return 0
+
+
+    def create_flow(self, packet):
+        id = packet.get_id()
+        flow = Flow(id)
+        flow.last_update = packet.timestamp
+        flow.active = False
+        
+        return flow
+
+
+
 
 
 class Flow:
     def __init__(self, id):
         self.id = id # Rule, souce_ip + dst_ip + src_port + dst_port
-        self.packets_count = 0
-        self.byte_count = 0
-        self.first_seen = None
         self.last_update = None
         self.active = True # if timeout, mark inactive
+        self.rules = []
 
+    def create_rule(self, create_time):
+        rule = Rule(create_time)
+        self.rules.append(rule)
+
+        return rule
+        
+
+class Rule:
+    def __init__(self, first_seen):
+        self.packets_count = 0
+        self.byte_count = 0
+        self.first_seen = first_seen  
+        self.last_update = None
 
 class Packet:
     def __init__(self, timestamp):
@@ -264,6 +303,7 @@ class Packet:
         self.udp_sport = None
         self.udp_dport = None
         self.size = 0
+        #Output.DEBUG(func=self.print_packet)
     
     def get_id(self):
         ports = ""
@@ -281,20 +321,7 @@ class Packet:
         print("Source IP: " + inet_to_str(self.ip_src))
         print("Dest IP: " + inet_to_str(self.ip_dst))
         print("Protocol: " + str(self.ip_protocol))
-        # if TCP: ...
         print("TCP Source port: " + str(self.tcp_sport))
         print("TCP Dest port: " + str(self.tcp_dport))
 
-
-        
-class Controller:
-    def create_flow(self, packet):
-        id = packet.get_id()
-        flow = Flow(id)
-        flow.packets_count += 1
-        flow.first_seen = packet.timestamp
-        flow.last_update = flow.first_seen
-        flow.active = True
-        
-        return flow
 
