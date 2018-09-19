2c3c413ec379c075b097db81228c66a3b04ee892
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2018-09-19 18:54:33 -0400

    Update generate test

--- generate_test.py
+++ generate_test.py
@@ -4,33 +4,79 @@ Timeout
 1. Timeout is meaningless when under 10ms, thus only consider timeout beyond 10ms. Same for large timeout.
 2. Reasonable range is from 10ms to 10s.
 3. Since using log axis, should sample small timeouts intensively, but with sparse large timeouts:
-    1. 10ms to 20ms; every 1ms
-    2. 20ms to 40ms; every 2ms
-    3. 40ms to 80ms; every 4ms
-    4. 80ms to 160ms; every 8ms
-    5. 160ms to 320ms; every 16ms
-    6. 320ms to 640ms; every 32ms
-    7. 640ms to 1280ms; every 64ms
-    8. 1280ms to 2560ms; every 128ms
-    9. 2560ms to 5120ms; every 256ms
-    10. 5120ms to 102400ms; every 512ms
+    1. 10ms to 100ms; every 10ms
+    2. 110ms to 290ms; every 20ms
+    3. 310ms to 670ms; every 40ms
+    4. 710ms to 1430ms; every 80ms
+    5. 1510ms to 2950ms; every 160ms
+    6. 3110ms to 5990ms; every 320ms
 
-Rules
+Multiplier (Maximum multiplier should less than 10s)
+    1: 1 to 100 every 10
+    2: 1 to 40 every 4
+    3: 1 to 20 every 2
+    4: 1 to 10 every 1
+    5: 1 to 4 every 0.4
+    6: 1 to 2 every 0.2
 
+Cache size: TBD
 
+Threshold:
+    2 to 5 every 1
 """
-
 
 import os
 import argparse
 
 if __name__ == '__main__':
-    # Get the user input
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('config_file', help="config file")
-    arg_parser.add_argument('--linear', 
-                    action="store_true", 
-                    help="Time axis linear or log")
-    args = arg_parser.parse_args()
-    config_file = args.config_file
-    linear = args.linear
+    # Location of tracefile:
+    trace_path = input("tracefile path: ")
+    start_id = int(input("start switch id (inclusive): "))
+
+    # Num of switches per file
+    x = 10
+
+    # Generate timeout list mentioned above
+    timeout_list = []
+    start = 10
+    for i in range(6):
+        cur_range = []
+        for j in range(10):
+            cur_range.append(start)
+            start = start + 10 * (2 ** i)
+        timeout_list.append(cur_range)
+
+    # Generate no_cache test files
+    num_file = 60 // x
+    for i in range(num_file):
+        file_name = "no_cache_config" + str(i) + ".txt"
+        cur_fp  = open(file_name, "w+")
+
+        cur_fp.write("[COMMON]\n")
+        cur_fp.write("trace_path=" + trace_path + "\n")
+        cur_fp.write("num_switches=" + str(x) + "\n")
+
+        for j in range(x):
+            cur_fp.write("[SW" + str(start_id) + "]\n")
+            start_id += 1
+
+            # Detect timeout:
+            total_index = i * x + j
+            row = total_index // 10
+            col = total_index % 10
+            cur_fp.write("timeout=" + str(timeout_list[row][col]) + "\n")
+    
+        cur_fp.close()
+
+            
+
+
+
+
+
+    
+
+
+
+
+
