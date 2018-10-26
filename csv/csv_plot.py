611722ddc11244fccf43c71308bfb1b08158515b
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2018-10-26 15:17:51 -0400

    v2

--- csv/csv_plot.py
+++ csv/csv_plot.py
@@ -21,8 +21,8 @@ packet_cat = ["max_packet", "99_packet", "95_packet", "mean_packet", "median_pac
 cache_cat = ["max_cache", "99_cache", "95_cache", "mean_cache", "median_cache"]
 
 # Find all file_cat
-file_cat = ["no_cache", "dynamic","fifo", "fixed_cache", "smart_time"]
-file_cat = ["fixed_cache"]
+file_cat = ["no_cache", "dynamic", "fixed_cache", "smart_time"]
+# file_cat = ["fixed_cache"]
 file_out = {}
 
 for category in file_cat:
@@ -37,19 +37,16 @@ for category in file_cat:
         multi = {}
         for i in range(N):
             cur_m = data[i, Q["multiplier"]]
-            if cur_m in multi.keys():
-                multi[cur_m].append(data[i, :])
-            else:
-                multi[cur_m] = [data[i, :]]
+            if cur_m == 1:
+                if cur_m in multi.keys():
+                    multi[cur_m].append(data[i, :])
+                else:
+                    multi[cur_m] = [data[i, :]]
         for cur_m in multi.keys():
             new_catname = category + str(cur_m)
             file_out[new_catname] = np.array(multi[cur_m])
             
-
-
-
-
-
+# Main table
 for cur_packet in packet_cat:
     fig, ax = plt.subplots()
     fig.set_size_inches(20, 15)
@@ -71,48 +68,23 @@ for cur_packet in packet_cat:
     plt.savefig(cur_packet + '_Summary')
     plt.close('all')
 
-for cur_packet in cache_cat:
+for cur_cache in cache_cat:
     fig, ax = plt.subplots()
     fig.set_size_inches(20, 15)
     for cur_cat in file_out.keys():
         data = file_out[cur_cat]
         hr = data[:, Q["all_hr"]]
-        packet = data[:, Q[cur_packet]]
+        cache = data[:, Q[cur_cache]]
 
         if linear:
-            ax.plot(hr, packet, label=cur_cat, marker='.')
+            ax.plot(hr, cache, label=cur_cat, marker='.')
         else:
-            ax.semilogx(hr, packet, label=cur_cat, marker='.')
+            ax.semilogx(hr, cache, label=cur_cat, marker='.')
 
     ax.legend(loc='upper left', shadow=True)
     ax.grid(which='both')
     plt.ylabel("Flow occupancy")
     plt.xlabel("Hit rate")
     plt.title("Summary")
-    plt.savefig(cur_packet + '_Summary')
+    plt.savefig(cur_cache + '_Summary')
     plt.close('all')
-
-
-fig, ax = plt.subplots()
-fig.set_size_inches(20, 15)
-for cur_cat in file_out.keys():
-    data = file_out[cur_cat]
-    hr = data[:, Q["all_hr"]]
-
-    cache = data[:, Q["mean_packet"]]
-    packet = data[:, Q["mean_cache"]]
-    ratio = cache / packet
-
-    if linear:
-        ax.plot(hr, ratio, label=cur_cat, marker='.')
-    else:
-        ax.semilogx(hr, packet, label=cur_cat, marker='.')
-
-ax.legend(loc='upper left', shadow=True)
-ax.grid(which='both')
-plt.ylabel("ratio")
-plt.xlabel("hr")
-plt.title("Summary")
-plt.savefig('_Summary')
-plt.close('all')
-        
